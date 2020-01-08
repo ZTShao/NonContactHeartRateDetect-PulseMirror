cbfc1c7eb867cd71394de4d44204c19967de1a67
Shao, Zhetai <zhetais@uci.edu>
2020-01-07 19:20:40 -0800

    config

--- openCVLibrary330/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary330/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /Users/shao/Desktop/ShaoShuaige/NonContactHeartRateDetect/openCVLibrary330/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
+ * Original file: /Users/shao/AndroidProjects/NonContactHeartRateDetect-PulseMirror/openCVLibrary330/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
